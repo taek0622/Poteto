716d2d47f23921acded25680a6a99fd9701edff1
taek0622 <min4644@gmail.com>
2024-04-02 06:41:01 +0900

    Init Project

---
+++ Poteto/PotetoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PotetoApp.swift
+//  Poteto
+//
+//  Created by 김민택 on 4/2/24.
+//
+
+import SwiftUI
+
+@main
+struct PotetoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
